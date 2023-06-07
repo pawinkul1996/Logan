4d838c2ef28de8dab2ff8ddf51458ba7556e805b
sarugmaklp8 <gsishkpagsh@gmail.com>
2023-06-07 15:10:10 +0800

    feat: staged update #7

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,3 +4,4 @@ import stakeRouter from './routes/stake.js';
 const app = express(); app.use(cors()); app.use(express.json()); app.use('/api/stake', stakeRouter);
 const PORT = process.env.PORT || 3001; app.listen(PORT, ()=> console.log('Backend running on', PORT));
 \n// touch 0
+\n// touch 6
